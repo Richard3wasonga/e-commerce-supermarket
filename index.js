f602090ee2748fcdc69c51701fde65c1f38d8454
richard <richard.wasonga@student.moringaschool.com>
2025-03-25 15:36:42 +0300

    cart update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server_url = "http://localhost:3000/items"
-let productList =[];
-const cartItems = [];
+
 function getId (id_name){
     return document.getElementById(id_name)
 }
@@ -44,49 +43,3 @@ function displayProducts(prod){
 
 }
 
-function addtocart(productId){
-    const product = productList.find(item => item.id === productId)
-    if (product){
-    let existingItem = cartItems.find(item => item.id === productId)
-    if(existingItem){
-        existingItem.quantity += 1
-    }else {
-        cartItems.push({...product,quantity: 1})
-    }
-    console.log(`product With Id ${productId} added to cart`)
-    alert("Product added to cart")
- }    
-}
-
-
-
-
-function displayItems(){
-    let cartContainer = getId("cart-container")
-    cartContainer.innerHTML =""
-    if(cartItems.length === 0){
-        cartContainer.innerHTML="<p>Your cart is empty.</P>"
-        return;
-    }
-    cartItems.forEach(item => {
-        let cartItem= createElement("div")
-        cartItem.classList.add("cart-item")
-        cartItem.innerHTML= `
-            <img src="${item.image}" alt="${item.name}">
-            <h4>${item.name}</h4>
-            <p>Price: $${item.price}</p>
-            <button onclick = "removefromcart(${item.id})">Remove</button>
-        `;
-        appendChild(cartContainer,cartItem)
-    })
-}
-function removefromcart(productId){
-    let index = cartItems.findIndex(item => item.id === productId)
-    if(index !== -1){
-        cartItems.splice(index,1)
-        displayItems()
-
-    }
-}
-let  cartButton = getId("cart");
-addEvent(cartButton,"click",displayItems)
