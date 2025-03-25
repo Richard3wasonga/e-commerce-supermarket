90383c5691422bacf4ef8021097258a19980115a
richard <richard.wasonga@student.moringaschool.com>
2025-03-25 10:35:07 +0300

    ongoing product display

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const server_url = "http://localhost:3000/items"
 function getId (id_name){
     return document.getElementById(id_name)
 }
+function createElement(element){
+    return document.createElement(element)
+}
 fetch(server_url,{
     method: "GET",
     headers:{
         "Content-Type" : "application/json",
-        "Accept" : "apllication/json"
+        "Accept" : "application/json"
     }
     
 })
@@ -16,14 +19,20 @@ fetch(server_url,{
 function displayProducts(prod){
     let product_caterory =getId('product-category')
     prod.forEach(item => {
-        let html=`
-        <div class="single-products">
-        <img><
-        <h4>${item.title}</h4>
-        
-        </div>
-        `
-        
+        let product_div = createElement("div")
+        product_div.classList.add("single-products")
+        product_caterory.innerHTML =`
+            <img src="${item.image}" alt="${item.title}">
+            <h4>${item.title}</h4>
+            <p>Price: $${item.price}</p>
+            <button onclick"addtocart(${item.id})">Add to cart</button>
+    `
+    product_caterory.appendChild(product_div)
     });
+    
 
+}
+function addtocart(procuctId){
+    console.log(`product With Id ${procuctId} added to cart`)
+    alert("Product added to cart")
 }
